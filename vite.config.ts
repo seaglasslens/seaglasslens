88825491370108f2005bca4a28fcdb5a9530bce5
Tyler Tran <tylerttran1997@gmail.com>
2024-11-23 03:34:48 -0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/seaglasslens",
+  base: "/seaglasslens.github.io",
   css: {
     preprocessorOptions: {
       scss: {
