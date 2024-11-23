8187c672ee1b384004e79c0f183139702717b701
Tyler Tran <tylerttran1997@gmail.com>
2024-11-23 05:38:41 -0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -14,12 +14,12 @@ import "./components/page-transition/PageTransition.scss";
 import "./components/artists/Artists.scss";
 import "./components/modal/Modal.scss";
 import "./components/toggle-bar/ToggleBar.scss";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
