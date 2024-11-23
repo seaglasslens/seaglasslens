27e474a89619e0813046a96197fca89168a7653f
Tyler Tran <tylerttran1997@gmail.com>
2024-11-23 03:58:35 -0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -18,7 +18,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/seaglasslens.github.io">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
